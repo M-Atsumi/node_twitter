3a4691beff496af0912054b0f09784b2c433cced
Masatoshi-A <anny_padme@yahoo.com>
2016-08-17 23:38:00 +0900

    スペース->tab

--- twstream.js
+++ twstream.js
@@ -3,10 +3,10 @@ var twitter = require('ntwitter');
 var config = require('./config.js');
 
 var twit = new twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
+	consumer_key: config.consumer_key,
+	consumer_secret: config.consumer_secret,
+	access_token_key: config.access_token_key,
+	access_token_secret: config.access_token_secret
 });
 
 var keyword = process.argv[2]; //第一引数
@@ -14,22 +14,22 @@ var option = {'track': keyword};
 
 var fs = require('fs');
 var app = require('http').createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(fs.readFileSync('index.html'));
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(fs.readFileSync('index.html'));
 }).listen(3000, 'localhost',function(){
-  console.log(keyword+'を含むツイートを取得します。');
+	console.log(keyword+'を含むツイートを取得します。');
 });
 
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
-  socket.on('msg', function(data) {
-    io.sockets.emit('msg', data);
-  });
+	socket.on('msg', function(data) {
+		io.sockets.emit('msg', data);
+	});
 });
 
 twit.stream('statuses/filter', option, function(stream) {
-  stream.on('data', function (data) {
-    io.sockets.emit('msg', data);
-    console.log(data);
-  });
+	stream.on('data', function (data) {
+		io.sockets.emit('msg', data);
+		console.log(data);
+	});
 });
