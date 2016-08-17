113d712c725db2740fcbebc561090ff7ebb89dc4
Masatoshi-A <anny_padme@yahoo.com>
2016-08-18 01:04:22 +0900

    風速機能実装

--- twstream.js
+++ twstream.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
 var util = require('util');
 var twitter = require('ntwitter');
+var http = require('http');
+var server = http.createServer();
 var config = require('./config.js');
 
 var twit = new twitter({
@@ -12,15 +15,34 @@ var twit = new twitter({
 var keyword = process.argv[2]; //第一引数
 var option = {'track': keyword};
 
-var fs = require('fs');
-var app = require('http').createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(fs.readFileSync('index.html'));
-}).listen(3000, 'localhost',function(){
-	console.log(keyword+'を含むツイートを取得します。');
-});
+server.on('request', doRequest);
+server.listen(3000);
 
-var io = require('socket.io').listen(app);
+function doRequest(request, response) {
+    switch(request.url) {
+    case '/':
+        fs.readFile('./index.html', 'UTF-8',
+            function (err, data) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+                response.end();
+            }
+        );
+        break;
+    case '/css/style.css':
+        fs.readFile('./css/style.css', 'UTF-8',
+            function (err, data) {
+                response.writeHead(200, {'Content-Type': 'text/css'});
+                response.write(data);
+                response.end();
+            }
+        );
+        break;
+    }
+};
+console.log(keyword+'を含むツイートを取得します。');
+
+var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket) {
 	socket.on('msg', function(data) {
 		io.sockets.emit('msg', data);
@@ -29,6 +51,7 @@ io.sockets.on('connection', function(socket) {
 
 twit.stream('statuses/filter', option, function(stream) {
 	stream.on('data', function (data) {
+		data.keyword = keyword;
 		io.sockets.emit('msg', data);
 		console.log(data);
 	});
